9bf809e8a4fd07331ba7a1073248f581df86ebf6
Karolis <karolisdarbinis2@gmail.com>
2023-12-22 13:30:56 +0200

    Update

--- vue-tuts/src/App.vue
+++ vue-tuts/src/App.vue
@@ -3,9 +3,24 @@
     <App-Header v-bind:name="name"></App-Header>
     <div>
       <h1>{{ title }}</h1>
+      <form-helper>
+        <div slot="form-header">
+          <h3>Title of the form</h3>
+        </div>
+        <div slot="form-fields">
+          <input type="text" placeholder="Name" required />
+          <input type="text" placeholder="Surname" required />
+        </div>
+        <div slot="form-controls">
+          <button v-on:click="submit">Submit</button>
+        </div>
+      </form-helper>
       <!-- Jei norime jog atributas butu dinaminis, turime naudoti v-bind: -->
       <Users v-bind:ninjas="ninjas"></Users>
     </div>
+    <component v-bind:is="component"></component>
+    <button v-on:click="component = 'form-one'">Show form one</button>
+    <button v-on:click="component = 'form-two'">Show form two</button>
     <App-Footer
       v-bind:name="name"
       v-on:changeName="updateName($event)"
@@ -14,22 +29,30 @@
 </template>
 
 <script>
+import formOne from "./components/formOne";
+import formTwo from "./components/formTwo";
 //importing localy
 // import Home from "./Home.vue";
 // import Header from "./components/Header.vue";
 // import Footer from "./components/Footer.vue";
-import { formHelper } from "./components/formHelper.vue";
+// import formHelper from "./components/formHelper";
+
 export default {
+  components: {
+    "form-one": formOne,
+    "form-two": formTwo
+  },
   // components: { users: Home },
   // components: {
   //   "app-header": Header,
   //   "app-footer": Footer
   // },
-  components: {
-    "form-helper": formHelper
-  },
+  // components: {
+  //   "form-helper": formHelper
+  // },
   data() {
     return {
+      pav: "Dynami slot title",
       title: "First app.",
       "list-name": "List of users",
       ninjas: [
@@ -40,7 +63,8 @@ export default {
         { name: "Kami", speciality: "Webpack", show: false },
         { name: "Yoshi", speciality: "Data Diggin", show: false }
       ],
-      name: "Users app"
+      name: "Users app",
+      component: "form-two"
     };
   },
   methods: {
--- vue-tuts/src/main.js
+++ vue-tuts/src/main.js
@@ -3,10 +3,16 @@ import App from "./App.vue";
 import Home from "./components/Home.vue";
 import Header from "./components/Header.vue";
 import Footer from "./components/Footer.vue";
+import formHelper from "./components/formHelper";
+// import formOne from "./components/formOne";
+// import formTwo from "./components/formTwo";
 
 Vue.component("Users", Home);
 Vue.component("App-Header", Header);
 Vue.component("App-Footer", Footer);
+Vue.component("form-helper", formHelper);
+// Vue.component("form-one", formOne);
+// Vue.component("form-two", formTwo);
 
 export const eventBus = new Vue();
 
